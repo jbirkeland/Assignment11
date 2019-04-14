46581ada558f4b325e40634a05a664226a189222
jordan birkeland <jbirkeland@mcad.edu>
2019-04-14 16:31:52 -0500

    Added remove button and removeInformation method

---
+++ script.js
@@ -0,0 +1,53 @@
+var myVue = new Vue({
+    el: "#app",
+    data: {
+        postcards: [
+            {
+                rank: 1,
+                location: "San Diego",
+                year: 2019,
+                landmark: "Sunset Cliffs",
+                description: "Architecture and scenery from the cliffs",
+            },
+            {
+                rank: 2,
+                location: "Arizona",
+                year: 2002,
+                landmark: "Grand Canyon",
+                description: "Best family vacation ever",
+            },
+            {
+                rank: 3,
+                location: "Banff",
+                year: 1995,
+                landmark: "Chateau Lake Louise",
+                description: "On the shores of Lake Louise",
+            },
+            {
+                rank: 4,
+                location: "Bar Harbor",
+                year: 2017,
+                landmark: "Acadia National Park",
+                description: "You have to try the lobster pizza",
+            },
+            {
+                rank: 5,
+                location: "North Shore",
+                year: 1999,
+                landmark: "Split Rock Lighthouse",
+                description: "The lighthouse sits majestically on the cliff",
+            }
+        ]
+    },
+    methods: {
+        removeInformation: function(informationObject){
+            this.postcards = this.postcards.filter(function(information){
+                if (information.rank !== informationObject.rank) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+        }
+    }
+});
